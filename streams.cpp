500258c754133ee32a6f5727c1dc087cc5fab15b
danielromeo <danielromeo99@gmail.com>
2020-09-23 11:09:39 +0200

    added streams

---
+++ streams.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "math.h"
+#include <vector>
+// #include <inStream>
+// #include <outStream>
+// using namespace std;
+#include <fstream>
+#include <ifstream>
+
+
+int main(){
+	
+	int theNumber;
+
+	ifSteam inStream;
+	ofStream outStream;
+
+	//outStream << "theNumber is" << theNumber << endl;
+	int x = 100;
+	std::cout << x << std::endl;	
+	
+
+    return 0;
+}
+      
