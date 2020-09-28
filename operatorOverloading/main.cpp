a970f7dec983f3ed5eb36c5338b82230ea8bfe1c
danielromeo <danielromeo99@gmail.com>
2020-09-28 23:00:17 +0200

    Added operator overloading

---
+++ operatorOverloading/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "math.h"
+#include <vector>
+using namespace std;
+
+
+
+int main(){
+
+    
+    return 0;
+}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
---
+++ smallProjects/assignment2/question3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "math.h"
+#include <vector>
+// #include <inStream>
+// #include <outStream>
+// using namespace std;
+#include <fstream>
+#include <string> 
+using namespace std;
+
+class ExamType{
+    public:
+    ExamType();
+    ExamType(string m, string v, int t, string d);
+
+    private:
+    string module;
+    string venue;
+    int time;
+    string date;
+};
+
+int main()
+{
+    if(){
+        
+    }
+
+    return 0;
+}
