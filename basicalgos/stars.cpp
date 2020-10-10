8722ce61f42363992b9ca8d97b01277ff90e5c07
danielromeo <danielromeo99@gmail.com>
2020-10-10 21:52:13 +0200

    update

---
+++ basicalgos/stars.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "math.h"
+#include <vector>
+#include <fstream>
+
+void triangle(int num){
+    int number = 5;
+    for(int i =0; i < 5;i++){
+
+        for(int j =0; j  < number; j++){
+            std::cout << "*";    
+        }
+        std::cout << std::endl;
+        number--;
+    }
+}
+
+void reverseTriangle(int num){
+    int number = 0;
+    for(int i =0; i < 5;i++){
+
+        for(int j =0; j  < number; j++){
+            std::cout << "*";    
+        }
+        std::cout << std::endl;
+        number++;
+    }
+}
+
+void oppositetriangle(int num){
+    int number = 5;
+    int spacenumber = 0;
+
+    for(int i =0; i < 5;i++){
+
+        for(int k = 0; k < spacenumber; k++){
+            std::cout << " ";
+        }
+
+        for(int j =0; j  < number; j++){
+            std::cout << "*";    
+        }
+        std::cout << std::endl;
+        number--;
+        spacenumber++;
+    }
+}
+
+void diomond(int num){
+    int number = 1;
+    int spacenumber = 5;
+    
+
+    for(int i =0; i < 5;i++){
+        for(int k = 0; k < spacenumber; k++){
+            std::cout << " ";
+        }
+        
+        for(int j = 0; j < number; j++){
+            std::cout << "*";    
+        }
+        std::cout << std::endl;
+        number+=2;
+        
+        spacenumber--;
+    }
+}
+
+int main(){
+	
+    //
+    // triangle(4);
+    // reverseTriangle(5);
+    // oppositetriangle(5);
+    diomond(5);
+    return 0;
+}
+      
---
+++ smallProjects/assignment2/question6/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include "math.h"
+#include <vector>
+#include <fstream>
+#include <string> 
+#include <limits>
+#include "phonecall.h"
+#include <fstream>
+
+using namespace std;
+
+typedef std::numeric_limits< double > dbl;
+
+int main()
+{   
+   Runner run()
+
+    return 0;
+}
