174c29bbcaeb60a7f8e63727eb1f3e1f5700a6ae
Daniel Mamphekgo <Danielromeo99@gmail.com>
2021-02-10 20:42:30 +0200

    Added algorithms, added jarvis march algorithm

---
+++ cpp/algorithms/jarvismarch.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cmath>
+#include <set>
+/*Algorithm */
+
+struct Coord{
+	int x;
+	int y;
+};
+
+float getGradient(std::pair<int, int> one, std::pair<int, int> two){
+	float result = (one.second-two.second)/(one.first-two.first);
+	return result;
+}
+
+int main(){
+
+	float val = getGradient(std::make_pair(0,0), std::make_pair(2, 12));
+	std::cout << "gradient is : "<<val << std::endl;
+
+	std::pair<int, int> coordspair;
+	std::set<std::pair<int, int>> coordsset;
+	std::set<std::pair<int, int>>::iterator it = coordsset.begin();
+	const int MAXRANGE = 1000;
+
+	//generate 10 numbers and create a pair  
+	srand((int)time(0));
+	int randomNumber;
+	int randomNumber2;
+	for (int i = 0; i < 50; ++i) // generate 5 coordinates
+	{
+		randomNumber = (rand()% MAXRANGE)+ 1;
+		randomNumber2 = (rand()% MAXRANGE)+ 1;
+		coordspair.first = randomNumber;
+		coordspair.second = randomNumber2;
+		coordsset.insert(coordspair);
+	}
+
+	std::cout << "coordinates are as follows: " << std::endl;
+	for (auto a : coordsset)
+	{
+		std::cout << "x: "<< a.first << "  y: " << a.second << std::endl; 
+	}
+
+	// Jarvis march
+	std::pair<int, int> mostSouthern(MAXRANGE,MAXRANGE);
+	for (auto a : coordsset)
+	{
+		if(a.second < mostSouthern.second){
+			mostSouthern.first = a.first;
+			mostSouthern.second = a.second;
+		}
+	}
+	std::cout << "Most southern point is : (" << mostSouthern.first << ","<< mostSouthern.second << ")" << std::endl;
+
+	return 0;
+}
