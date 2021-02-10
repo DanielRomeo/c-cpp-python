c887721ae5f0c622e5ff9ca76e9e8c46c4364547
Daniel Mamphekgo <Danielromeo99@gmail.com>
2021-02-11 00:20:08 +0200

    completed jarvis algo but it has a major bug

--- cpp/algorithms/jarvismarch.cpp
+++ cpp/algorithms/jarvismarch.cpp
@@ -4,33 +4,40 @@
 #include <string>
 #include <cmath>
 #include <set>
-/*Algorithm */
-/*The algorithm ...*/
-struct Coord{
-	int x;
-	int y;
-};
+#include <queue>
 
-float getGradient(std::pair<int, int> one, std::pair<int, int> two){
-	float result = (one.second-two.second)/(one.first-two.first);
+/*Algorithm */
+/*	The algorithm ...
+	A bettwe way i could have written this is if i had used structs for coordinates and lists
+*/
+void printQue(std::queue<std::pair<int, int>> que){
+	while(!que.empty()){
+		std::cout << que.back().first << " " << que.back().second << std::endl;
+		que.pop();
+	}
+}
+
+double getGradient(std::pair<int, int> one, std::pair<int, int> two){
+	double result = (one.second-two.second)/(one.first-two.first);
 	return result;
 }
 
 int main(){
 
-	float val = getGradient(std::make_pair(0,0), std::make_pair(2, 12));
-	std::cout << "gradient is : "<<val << std::endl;
+	// float val = getGradient(std::make_pair(0,0), std::make_pair(2, 12));
+	// std::cout << "gradient is : "<<val << std::endl;
 
+	std::queue<std::pair<int, int>> que;
 	std::pair<int, int> coordspair;
 	std::set<std::pair<int, int>> coordsset;
 	std::set<std::pair<int, int>>::iterator it = coordsset.begin();
-	const int MAXRANGE = 15;
+	const int MAXRANGE = 5;
 
 	//generate 10 numbers and create a pair  
 	srand((int)time(0));
 	int randomNumber;
 	int randomNumber2;
-	for (int i = 0; i < 10; ++i) // generate 5 coordinates
+	for (int i = 0; i < 3; ++i) // generate 5 coordinates
 	{
 		randomNumber = (rand()% MAXRANGE)+ 1;
 		randomNumber2 = (rand()% MAXRANGE)+ 1;
@@ -56,7 +63,29 @@ int main(){
 	}
 	std::cout << "Most southern point is : (" << mostSouthern.first << ","<< mostSouthern.second << ")" << std::endl;
 
+	que.push(mostSouthern);
+	for(auto a: coordsset){ // were looping the duration of coordset = eg 10
+		
+		std::pair<int, int> coordWithSmallestGradient = a;	
+		
+		for(auto b: coordsset){
+			if( (b == que.back()) || a == que.back() ){ // 
+				continue;
+			}
+			// if( (b.second == que.back().second)  || (a.second == que.back().second)){
+			// 	continue;
+			// }
 
-
+			if( (getGradient(que.back(), b) ) < (getGradient(que.back(), a)) ){
+				coordWithSmallestGradient = b;
+			}
+			// std::cout << que.back().first << " " << que.back().second  << std::endl;
+			// std::cout << getGradient(que.back(), b) << std::endl;
+			// continue;
+		}
+		que.push(coordWithSmallestGradient);
+	}
+	printQue(que);
+	// getGradient(std::make_pair(8, 9), std::make_pair(8, 9));
 	return 0;
 }
