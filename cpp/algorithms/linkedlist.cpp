7e6b8317c876b6bacef4caec5cb25aa8331cca00
Daniel Mamphekgo <Danielromeo99@gmail.com>
2021-02-11 20:16:59 +0200

    Added node to beginning linkedlist

--- cpp/algorithms/linkedlist.cpp
+++ cpp/algorithms/linkedlist.cpp
@@ -9,6 +9,8 @@ struct Node
 	Node* link = NULL;
 };
 
+Node* HeadNode; // this is the head node
+
 // function to add a item to end of linkedlist:
 void addToList(Node* Head, Node* Item){
 	Node* tempAddress = Head;
@@ -26,20 +28,25 @@ void printList(Node* Head){
 	}
 }
 
+void addToBeginning(Node* Head, Node* Item){
+	Item->link = Head;
+	HeadNode = Item;
+}
+
 int main(){
 	
 
-	Node* A; // this is the head node
-	A = NULL;
+	
+	HeadNode = NULL;
 	Node * temp = new Node();
 	Node * temp3 = new Node();
 	Node * temp2 = new Node();
 	Node * temp4 = new Node();
 	
 
-	temp->data = NULL;
+	temp->data = 1994;
 	temp->link = NULL;
-	A = temp;
+	HeadNode = temp;
 
 	temp2->data = 1600;
 	temp2->link = NULL;
@@ -50,11 +57,17 @@ int main(){
 	temp4->data = 100;
 	temp4->link = NULL;
 	
-	addToList(A, temp2);
-	addToList(A, temp3);
-	addToList(A, temp4);
+	addToList(HeadNode, temp2);
+	addToList(HeadNode, temp3);
+	addToList(HeadNode, temp4);
 
-	printList(A);
+	//add node to beginning...
+	Node* newHeadNode = new Node();
+	newHeadNode->data = 0;
+	newHeadNode->link = NULL;
+	addToBeginning(HeadNode, newHeadNode);
+
+	printList(HeadNode);
 
 	
 	return 0;
