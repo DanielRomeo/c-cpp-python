4534bf9d5502d3a384123ec08c19825a58838b41
danielromeo <danielromeo99@gmail.com>
2021-06-17 23:25:14 +0200

    started writing List data structure definitions

--- cpp/dataStructures/arrayBasedLists/List.cpp
+++ cpp/dataStructures/arrayBasedLists/List.cpp
@@ -1,2 +1,27 @@
 #include "List.h"
 
+List::List(){
+
+}
+
+bool List::isEmpty()const{
+
+}
+
+bool List::isFull()const{
+
+}
+
+int List::maxListSize()const{
+
+}
+
+void List::print()const{
+
+}
+
+template <class Type>
+bool List::isItemAtEqual(int location, const Type& item  ){
+
+}
+
