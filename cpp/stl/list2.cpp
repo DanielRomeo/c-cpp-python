d8d8a3176eacccfb81f2a1f15ccf1627a5836d0b
danielromeo <danielromeo99@gmail.com>
2021-07-04 22:27:11 +0200

    added the swap function to list2 stl

--- cpp/stl/list2.cpp
+++ cpp/stl/list2.cpp
@@ -3,7 +3,23 @@
 #include <iterator>
 #include <vector>
 
+void swaplists(std::list<int> listA, std::list<int> listB){
+	std::swap(listA, listB);
+	
+	// print the two lists
+	std::cout << "list A: " << std::endl;
+	for(auto i: listA){
+		std::cout << i << ", ";
+	}
+	std::cout << std::endl;
 
+	// pront the second list:
+	std::cout << "list B: " << std::endl;
+	for(auto i: listB){
+		std::cout << i << ", ";
+	}
+	std::cout << std::endl;
+}
 
 void printvector(std::vector<int> myvector){
 	for(auto i : myvector){
@@ -25,11 +41,20 @@ int main(){
 
 	std::list<int> list = {2,5,1};
 	std::list<int> list2 = {100,12};
-	list2 = list;
-	printlist(list2);
+	// list2 = list;
+	// printlist(list2);
+
+	// std::cout << list2.front() << std::endl;
+	// std::cout << list2.back() << std::endl;
+
 
 	std::vector vec1 = {1, 2, 3};
 	// printvector(vec1);
+
+
+	// swapping contents of two lists: and print the contents of both lists:
+	// std::swap(list2, list);
+	swaplists(list, list2);
 
 	return 0;
 }
