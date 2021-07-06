71d0a9b4e8bf3f59f8f07f57a754a801c21d389a
danielromeo <danielromeo99@gmail.com>
2021-07-07 00:30:46 +0200

    added rotate stl fi;le

---
+++ cpp/algorithm-stl/rotate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+// function that takes a vector and rotates it: takes a number of the elements and places them at the end:
+void rotateVector(std::vector<int> vec){
+	
+}	
+
+// function that does what the above does but reverses the contents of a vector:
+
+int main(){
+
+	// 
+
+	return 0;
+}
