3e8a0993c866608dea0630afe45136c659a49056
danielromeo <danielromeo99@gmail.com>
2021-07-07 00:58:37 +0200

    successfully rotated and reversed a vector4

--- cpp/algorithm-stl/rotate.cpp
+++ cpp/algorithm-stl/rotate.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <list>
+
+// function that inserts numbers 1 to 100 into the vector:
+void insertintoVector(std::vector<int> &vec){
+	for(int i = 0; i < 10; i++){
+		vec.push_back(i);
+	}
+}
+void printvector(std::vector<int> vec){
+	std::vector<int>::iterator ptr;
+	for(ptr = vec.begin(); ptr < vec.end(); ptr++){
+		std::cout << *ptr << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void reverseVector(std::vector<int> &vec){
+	std::reverse(vec.begin(), vec.end());
+	std::cout << "Succefully reversed the vector" << std::endl;
+}
+
 
 // function that takes a vector and rotates it: takes a number of the elements and places them at the end:
-void rotateVector(std::vector<int> vec){
-	
+void rotateVector(std::vector<int> &vec){
+	std::rotate(vec.begin(), vec.begin()+3, vec.end() );
 }	
 
-// function that does what the above does but reverses the contents of a vector:
+
+
+void rotateList(){
+
+}
+
 
 int main(){
 
-	// 
+	// Instantiation:
+	std::list<int> mylist = {1200,150,800};
+	std::vector<int> myvector = {};
+
+	insertintoVector(myvector);
+	printvector(myvector);
+	reverseVector(myvector);
+	printvector(myvector);
 
 	return 0;
 }
