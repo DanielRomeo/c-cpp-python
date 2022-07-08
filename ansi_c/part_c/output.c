064b0c984c301f0464fd684d46047b32b4c8b330
danielromeo <danielromeo99@gmail.com>
2022-07-08 22:06:58 +0200

    completed the output.c side of things

--- ansi_c/part_c/output.c
+++ ansi_c/part_c/output.c
@@ -3,6 +3,30 @@
 
 
 int main(){
+    // putchar();
     putchar('x');
+    putchar('\n');
+    putchar('K');
+
+    // puts(addr); outputs string and newline charector
+    puts("Hello world");
+    puts("hi");
+
+    // int errcount = 0;
+    // if(puts(cptr) == EOF){
+    //     errcount++;
+    // }
+
+    // printf is a routine to print formatted output to the standard output:
+    unsigned u = 100;
+    int i = 10;
+    char c = 'F';
+    
+
+    printf("%s %s", "I am a hero", "I am rich");
+    printf("%c", 'k');
+    puts("");
+    printf("My unsigned integer is %u and my signed integer is %i , and my charector is %c , and name is: %s", u,i, c, "Daniel");
+
     return 0;
 }
