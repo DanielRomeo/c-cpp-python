6615f5e0bfd9fbaff563d2cdc05c8cb33323a93e
danielromeo <danielromeo99@gmail.com>
2022-07-20 15:56:34 +0200

    adding inputs from ansi c

--- ansi_c/part_c/input.c
+++ ansi_c/part_c/input.c
@@ -3,7 +3,23 @@
 
 int main(){
 
-    
+    // simple program to enter a char and outputs it.
+    // printf("Enter a charector...\n");
+    // char c = getchar();
+    // printf("%c",c);
+
+    // simple program that allows user to keep entering chars untill it recognizes end of line:
+    // printf("Enter your details and add a space at the end...\n");
+    // char ch;
+    // while( (ch = getchar()) != '\n'){
+    //     printf("Charector you have just entered is: %c\n", ch);
+    // }
+
+    /*Program that asks a user to enter their name and it outpts their name using gets()*/
+    char str[50];
+    printf("Enter your name please?\n");
+    gets(str);
+    printf("Your name is : %s\n", str);
 
     return 0;
 }
--- c/pointers/main.c
+++ c/pointers/main.c
@@ -8,18 +8,28 @@ int main(){
 
 	// printf("What year is it?");
 	// scanf("%d", &year);
-
 	// point = &year;
 	// *point = 600; // dereferncing the pointer, now its value has changed
-
 	// printf("\nThe year is %d", year); // print the actual number
 	// printf("\n The year's address is %p", &year); // print te address of the pointer
 	// printf("\n The point's address is %p", &point); // this prints out the pointers address
-
 	// printf("\nHello\n");
-	
 
-	puts();
-	printf("hello mate");
+	// practice session 2:
+	// int * p = NULL; //  pointer points to a null value
+	// int number = 200;
+	// p = &number;
+	// *p = 500;
+	// printf("the value in P is: %d and the address of p is: %p \n",*p, p );
+
+	// // printing the size of an integer:
+	// printf("the size of number is : %d bytes\n", sizeof(int));
+
+	// void pointers, pointer arithmetic and pointer types:
+	//size of char and size of integer?
+	printf("Size of char is %d and size of integer is %d",sizeof(char), sizeof(int));
+
+
+
 	return 0;
 }
