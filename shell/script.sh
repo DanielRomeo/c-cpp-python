0c1babba70462203f1f9fe3a79f2cbd7bb798cc1
danielromeo <danielromeo99@gmail.com>
2022-08-19 15:44:14 +0200

    added bash scripting

--- shell/script.sh
+++ shell/script.sh
@@ -1,19 +1,29 @@
 #! /bin/bash
+# NOTE!!! BASH is case and space sensitive...
 
 # ECHO COMMAND  
-echo Hello world!
+# echo Hello world!
 
 # variables
 # uppercase by convetion, can start with letters, numbers and underscores
-NAME="ROMEO"
-echo "My name is ${NAME}"
+NAME="Romeo"
+SURNAME="Mamphekgo"
+# echo "My name is ${NAME}"
 
 # get user input
-read -p "Enter your surname: " SURNAME
+# read -p "Enter your surname: " SURNAME
 echo "Hello $SURNAME, nice to meeet you!"
 
 # if statment
-# if ["$SURNAME" == "Mamphekgo"]
+# if [ "$SURNAME" == "Mamphekgo" ]
 # then
 #     echo "Your surname is Mamphekgo"
 # fi
+
+# if else
+if [ "$SURNAME" == "Mamphekgo" ]
+then
+    echo "Your surname is Mamphekgo"
+else
+    echo "Your surname is not Mamphekgo"
+fi
