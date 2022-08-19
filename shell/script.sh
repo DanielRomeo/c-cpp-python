1bc886af113ef45c80c5af1b8f8117704abcc76d
danielromeo <danielromeo99@gmail.com>
2022-08-19 15:34:05 +0200

    first ever shell program

--- shell/script.sh
+++ shell/script.sh
@@ -5,5 +5,15 @@ echo Hello world!
 
 # variables
 # uppercase by convetion, can start with letters, numbers and underscores
-NAME = "ROMEO"
-echo "My name is $NAME"
+NAME="ROMEO"
+echo "My name is ${NAME}"
+
+# get user input
+read -p "Enter your surname: " SURNAME
+echo "Hello $SURNAME, nice to meeet you!"
+
+# if statment
+# if ["$SURNAME" == "Mamphekgo"]
+# then
+#     echo "Your surname is Mamphekgo"
+# fi
