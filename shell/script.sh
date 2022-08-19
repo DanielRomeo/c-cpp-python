44c025184ebb8c84962e08c0394b9501261752c3
danielromeo <danielromeo99@gmail.com>
2022-08-19 15:26:41 +0200

    started bash folder

---
+++ shell/script.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# ECHO COMMAND  
+echo Hello world!
+
+# variables
+# uppercase by convetion, can start with letters, numbers and underscores
+NAME = "ROMEO"
+echo "My name is $NAME"
