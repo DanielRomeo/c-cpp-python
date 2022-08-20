f7aeaa0ca3cc98942d01873834d136a1b7d5d6da
danielromeo <danielromeo99@gmail.com>
2022-08-20 10:19:20 +0200

    minor changes

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -5,7 +5,11 @@
 
 int main(){
 	
-	
+	for(int i = 0; i < 1000000; i++){
+		i *= 3000;
+		i+= 2000-i;
+		std::cout << i << std::endl;
+	}
 		
 	return 0;
 }
--- shell/script.sh
+++ shell/script.sh
@@ -7,12 +7,12 @@
 # variables
 # uppercase by convetion, can start with letters, numbers and underscores
 NAME="Romeo"
-SURNAME="Mamphekgo"
+SURNAME="fR"
 # echo "My name is ${NAME}"
 
 # get user input
 # read -p "Enter your surname: " SURNAME
-echo "Hello $SURNAME, nice to meeet you!"
+echo "Hello $NAME, nice to meeet you!"
 
 # if statment
 # if [ "$SURNAME" == "Mamphekgo" ]
@@ -24,6 +24,9 @@ echo "Hello $SURNAME, nice to meeet you!"
 if [ "$SURNAME" == "Mamphekgo" ]
 then
     echo "Your surname is Mamphekgo"
+elif [ "$SURNAME" == "Jack" ]
+then
+    echo "Your surname is Jack"
 else
-    echo "Your surname is not Mamphekgo"
+    echo "Your surname is NEITHER"
 fi
