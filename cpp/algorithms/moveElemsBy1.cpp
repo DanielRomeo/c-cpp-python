0909be543441a7ac2b1efffc10308b25cfdbe861
danielromeo <danielromeo99@gmail.com>
2024-02-04 16:31:25 +0200

    added movelemsby1

---
+++ cpp/algorithms/moveElemsBy1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+
+void printVector(std::vector<int> vec){
+    
+    for(int i = 0; i < vec.size(); i++){
+        std::cout << vec[i] << std::endl;
+    }
+}
+
+// Function that moves elements to the right 1 space:
+void moveByOne(std::vector<int> vec){
+    int j = 0;
+    for(int i = 0; i < vec.size(); i++){
+        if(vec[i] == 0){
+            j=i;
+            while(j >= 0 ){
+                vec[j] = vec[j-1];
+                j--;
+            }
+        }
+    }
+    printVector(vec);
+}
+
+int main(){
+
+    std::vector<int> vec =  {1,2,3,4,5,0,6,7,8};
+    moveByOne(vec);
+    // printVector(vec);
+
+    return 0;
+}
