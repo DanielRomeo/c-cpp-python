00fac7e23b71cb12d7ac0ce30cd7598af4b471dc
danielromeo <danielromeo99@gmail.com>
2024-02-25 20:04:00 +0200

    created forTutorial for vectors

--- cpp/iterators/forTutorial.cpp
+++ cpp/iterators/forTutorial.cpp
@@ -15,8 +15,30 @@ int main()
 
 
     std::list<int> mylist = {1,2,3,50  ,5,6};
-    for(const auto &element: mylist){
+    for(const auto element: mylist){
         std::cout << element  << std::endl;
+    }
+
+    std::cout << std::endl;
+    
+    std::list<int> c1 = {3, 100};
+    
+    // print the list:
+    for(const auto &elem : c1){
+        std::cout << elem << std::endl;
+    }
+
+    // insert new element at position index 1:
+    auto it2 = c1.begin();
+    it2++;
+    // std::cout << *it2 << std::endl;
+    c1.insert(it2, 500);
+
+    // print the list:
+    std::cout << std::endl;
+    std::cout << "New list is :" << std::endl;
+    for(const auto &elem : c1){
+        std::cout << elem << std::endl;
     }
 
 
---
+++ cpp/vectors/forTutorial.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+/* Vector is an array like container. 
+
+std::vector operations to test:
+- standard traversal
+- traversal with iterators
+
+std::vector methods to test:
+- push_back()
+- pop_back()
+- insert()
+- capacity()
+- size()
+
+*/ 
+
+// function to traverse through a vector:
+void traverseVector( std::vector<int> const &myVector){
+    for(auto a: myVector){
+        std::cout << a << std::endl;
+    }
+}
+
+// function to traverse vector through iterators:
+void traverseVectorWithIterator( std::vector<int> const &myVector){
+    std::vector<int>::iterator it;
+    for(auto it = myVector.begin(); it != myVector.end(); ++it){
+        std::cout << *it << std::endl;
+    }
+}
+
+// function to test capacity of vectors:
+void testVectorCapacity(std::vector<int>  &myVector){
+    // std::cout << "Capacity of vector is : "<< myVector.capacity() << std::endl;
+
+    // for loop to keep adding items in the vector and comparing the capacity:
+    for(int i = 0; i < 5; i++){
+        myVector.push_back(i);
+        std::cout << "Size of vector is : "<< myVector.size() << std::endl;
+        std::cout << "Capacity of vector is : "<< myVector.capacity() << std::endl;
+        
+    }
+    std::cout << "RUNNING SHRINK TO FIT..." << std::endl;
+    myVector.shrink_to_fit();
+    std::cout << "Size of vector is : "<< myVector.size() << std::endl;
+    std::cout << "Capacity of vector is : "<< myVector.capacity() << std::endl;
+}
+
+int main(){
+
+    std::vector<int> myVector = {};
+    // traverseVector(myVector);
+    // traverseVectorWithIterator(myVector);
+    testVectorCapacity(myVector);
+
+    return 0;
+}
