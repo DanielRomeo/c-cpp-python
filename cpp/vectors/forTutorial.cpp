e63c642c0007731fbb69833ce42a52d9dd0d626e
danielromeo <danielromeo99@gmail.com>
2024-02-27 10:59:13 +0200

    added method in comments of forTutorial vectors

--- cpp/vectors/forTutorial.cpp
+++ cpp/vectors/forTutorial.cpp
@@ -6,6 +6,7 @@
 std::vector operations to test:
 - standard traversal
 - traversal with iterators
+- test vector size and capacity
 
 std::vector methods to test:
 - push_back()
@@ -13,6 +14,7 @@ std::vector methods to test:
 - insert()
 - capacity()
 - size()
+- shrink_to_fit()
 
 */ 
 
