c91d2fc872e7053fef3f7d73c9c15cc327b8fbf6
daniel romeo <danielromeo99@gmail.com>
2024-04-03 20:15:21 +0200

    found all K values

--- cpp/chess/test.cpp
+++ cpp/chess/test.cpp
@@ -17,7 +17,7 @@ void printBoard(std::vector<std::vector<std::string>> &board){
 // create a 2d matrice with vectors to create a chess board:
 std::vector<std::vector<std::string>> createBoard(){
 	std::vector<std::vector<std::string>> myvec;
-	std::vector<std::string> strVec = {"x", "x","x", "x","x", "x","x", "x"};
+	std::vector<std::string> strVec = {"o", "o","o", "o","o", "o","o", "o"};
 	for(auto i = 0; i < 8; ++i){
 		myvec.push_back(strVec);
 	}
@@ -25,10 +25,31 @@ std::vector<std::vector<std::string>> createBoard(){
 }
 
 std::vector<std::vector<std::string>> addPieceAtPos(std::vector<std::vector<std::string>> &board, int row, int col){
-    board[row][col] = "S";
+    board[row][col] = "K";
     return board;
 }
 
+/* Write a function that finds the K value(knight) and then marks all possible jumps with a X*/
+void printPossibleJumps(std::vector<std::vector<std::string>> &board){
+    // std::vector<std::vector<std::string>>::iterator it = board.begin();
+    
+    // while(it != board.end()){
+    //     std::cout << 'a' <<std::endl;
+    // }
+    std::string value = "K";
+    // while(){
+
+    // }
+    for(auto i = 0; i < board.size(); ++i){
+        for(auto j =0; j < board[i].size(); ++j){
+            if(board[i][j] == value){
+                //
+                std::cout << "found one"<< std::endl;
+            }
+        }
+    }
+
+}
 
 
 
@@ -36,7 +57,10 @@ int main(){
 	std::vector<std::vector<std::string>> board = createBoard();
     printBoard(board);
     addPieceAtPos(board, 5,5);
+    addPieceAtPos(board, 4,2);
     printBoard(board);
+
+    printPossibleJumps(board);
 
 	return 0;
 }
