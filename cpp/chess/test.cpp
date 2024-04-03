d22a1b2baf439d4d60c807d64afc3279dabc3fa0
daniel romeo <danielromeo99@gmail.com>
2024-04-03 19:46:42 +0200

    created board and created a variable that represents a piece

--- cpp/chess/test.cpp
+++ cpp/chess/test.cpp
@@ -3,44 +3,40 @@
 #include <vector>
 
 
-// print the matrice:
-void print(std::vector<std::vector<std::string>> vec){
-	for(auto a: vec){	
+// print the chess board:
+void printBoard(std::vector<std::vector<std::string>> &board){
+	for(auto a: board){	
 		for(auto b: a){
 			std::cout << b;
 		}
 		std::cout << '\n';
-		
 	}
+    std::cout << "\n";
 }
 
-// create a 2d matrice with vectors:
+// create a 2d matrice with vectors to create a chess board:
 std::vector<std::vector<std::string>> createBoard(){
-	// std::vector<std::string> innerVec;
 	std::vector<std::vector<std::string>> myvec;
-
 	std::vector<std::string> strVec = {"x", "x","x", "x","x", "x","x", "x"};
-
 	for(auto i = 0; i < 8; ++i){
 		myvec.push_back(strVec);
 	}
-
-	print(myvec);
-
 	return myvec;
 }
 
-// void populateBoard(std::vector<std::vector<std::string>> &board){
-// 	// std::vector<std::vector<string>>::iterator it;
+std::vector<std::vector<std::string>> addPieceAtPos(std::vector<std::vector<std::string>> &board, int row, int col){
+    board[row][col] = "S";
+    return board;
+}
 
-// 	// for(it = board.begin(); it< board.end(); ++it){
-
-// 	// }
-// }
 
 
 
 int main(){
-	createBoard();
+	std::vector<std::vector<std::string>> board = createBoard();
+    printBoard(board);
+    addPieceAtPos(board, 5,5);
+    printBoard(board);
+
 	return 0;
 }
