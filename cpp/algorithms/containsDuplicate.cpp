95c1437b2e0b41aaaeee6c7d3329873426717ffb
danielromeo <danielromeo99@gmail.com>
2024-04-24 11:19:25 +0200

    added iterative function of containsDuplicate

--- cpp/algorithms/containsDuplicate.cpp
+++ cpp/algorithms/containsDuplicate.cpp
@@ -5,10 +5,26 @@
 // the program checks if the array(vector) contains duplicates numbers in it. 
 // if duplicates are found, then 1(true) will be printed, else, 0(false) will be printed.
 
+bool containsDuplicateIterative( std::vector<int> const &nums){
+    int current = nums[0];
+    for(auto i = 0; i < nums.size(); i++){
+        current = nums[i];
+        for(auto j = i+1; j < nums.size() ; j++){ // the value of J is very important
+            if(current == nums[j]){
+                std::cout << current << "...and nums is : " << nums[j] << std::endl;
+                return true;
+                
+            }
+        }
+    }
+    return false;
+
+}
+
 bool containsDuplicate(std::vector<int> &nums){
     std::unordered_set<int> myset;
     for(int num: nums){
-        if(myset.count(num)){
+        if(myset.count(num)){ // checks if the element exits in the set or not
             return true;
         }else{
             myset.insert(num);
@@ -20,7 +36,7 @@ bool containsDuplicate(std::vector<int> &nums){
 int main(){
 
     std::vector<int> myNumbersVector = {1,2,3,4,5,100, 100};
-    std::cout << containsDuplicate(myNumbersVector) << std::endl;
+    std::cout << containsDuplicateIterative(myNumbersVector) << std::endl;
 
     return 0;
 }
