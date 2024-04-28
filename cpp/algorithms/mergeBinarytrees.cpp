42eb28e66e88be30c8e341bc10db57b58594af29
danielromeo <danielromeo99@gmail.com>
2024-04-28 18:39:37 +0200

    successfully added the mergeBInaryTree algorithm

--- cpp/algorithms/mergeBinarytrees.cpp
+++ cpp/algorithms/mergeBinarytrees.cpp
@@ -4,11 +4,12 @@ struct Node{
     int data;
     Node* left ;
     Node* right;
+    Node(int data): data(data), left(nullptr), right(nullptr){}; // declaration initializer
 };
 
 Node* createNode(int value){
-    Node* newNode = new Node();
-    newNode->data = value;
+    Node* newNode = new Node(value);
+    // newNode->data = value;
     return newNode;
 }
 
@@ -36,17 +37,32 @@ void traverseNode(Node * root){
 
 // merge and print tree:
 Node * mergeTree(Node * tree1, Node * tree2){
-    Node * tree3 = nullptr;
-    if(tree1 == nullptr){
-        // return tree
+    // std::cout << "the merge runs " << std::endl;
+
+    if(tree1 == NULL){
+        // std::cout << "tree 1 is now NULL" << std::endl;
+        return tree2;
     }
+    if(tree2 == NULL){
+        // std::cout << "tree 2 is now NULL" << std::endl;
+        return tree1;
+    }
+
+    int values = tree1->data + tree2->data;
+    Node * tree3 = new Node(values);
+
+    std::cout << "Tree 3 value is : " << tree3->data << " ..." << std::endl;
+
+    tree3->left = mergeTree(tree1->left, tree2->left);
+    tree3->right = mergeTree(tree1->right, tree2->right);
+
+    return tree3;
 
 }
 
 int main(){
-
     Node * myroot1 = insertNode(myroot1, 15);
-    Node * myroot2 =  insertNode(myroot2, 12);
+    Node * myroot2 =  insertNode(myroot2, 20);
 
     // insert into the first tree:
     insertNode(myroot1, 10);
@@ -61,6 +77,8 @@ int main(){
     traverseNode(myroot2);
 
     // run merge and print:
+    Node * newTree = mergeTree(myroot1, myroot2);    
+    // traverseNode(newTree);
 
     return 0;
 }
