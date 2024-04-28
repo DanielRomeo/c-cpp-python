b45b7f633c28dbbc7948b929b8e2597bdc62e922
danielromeo <danielromeo99@gmail.com>
2024-04-28 18:11:58 +0200

    added constructor declaration initialization of variables

--- cpp/structs/structs.cpp
+++ cpp/structs/structs.cpp
@@ -18,25 +18,31 @@ struct employeeType{
 	float salary;
 };
 
-// write a void funcion that recieves the array name and outputs the data:
-// void displayNames(const nameType & nameP){
-// 	for (int i = 0; i < 5; ++i)
-// 	{
-// 		std::cout << nameP[i].first << " " << nameP[i].last;
-// 	}
-// }
+// create a Car struct with a constructor:
+struct Car{
+	std::string name;
+	std::string make;
+	// member initialzer allows me to initialize the member variables of a class or stuct directly in the constructor declaration
+	Car(std::string name, std::string make): name(name), make(make) {}; 
+};
 
 int main(){
 
 	employeeType employees[100];
 	employeeType newEmployee;
 
-	// newEmployee.name.first = "dan";
-	// newEmployee.name.last = "mama";
+	newEmployee.name.first = "dan";
+	newEmployee.name.last = "mama";
 	newEmployee = employees[22]; // copy one into the other:
 	employees[50].salary += 7.5; // add a value to something:
 
-	nameType names[5];
+	// nameType names[5]; // I guess you could do this if you wanted, why though, no one knows.
+
+	// create a car pointer:
+	Car * mycar = new Car("Focus", "Ford"); // I just created a pointer myCar that points to my car
+	std::cout << mycar->make << std::endl;	
+
+
 
     return 0;
 }
