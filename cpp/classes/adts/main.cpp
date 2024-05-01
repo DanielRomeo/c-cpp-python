eacd0330ece84604f91f68586891aa9c4bf25960
Daniel <Danielromeo99@gmail.com>
2024-05-01 23:55:09 +0200

    Update main.cpp

--- cpp/classes/adts/main.cpp
+++ cpp/classes/adts/main.cpp
@@ -9,14 +9,8 @@
 int main(){
 
 	User daniel("daniel", 15);
-    // string name;
-
-    // cout << "Enter name and age" << endl;
-    // cin >> name;
-    // cout << "Your name is " << name << endl;
-
-    daniel.setDetails("sa", 15);
-    daniel.getDetails();
+   daniel.setDetails("sa", 15);
+   daniel.getDetails();
 
     return 0;
 }
