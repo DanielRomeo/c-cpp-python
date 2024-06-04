4cf20d668322e4b4ad7c13779ba127825db1647a
danielromeo <danielromeo99@gmail.com>
2024-06-04 11:27:41 +0200

    added print and initilize list methods to the linked list adt

--- cpp/dataStructures/linkedList/malikClassExample/linkedlist.cpp
+++ cpp/dataStructures/linkedList/malikClassExample/linkedlist.cpp
@@ -14,3 +14,41 @@ linkedList<Type>::linkedList(){
     count = 0;
 }
 
+// destructor:
+template<class Type>
+void linkedList<Type>::destroyList(){
+    // create a temporary pointer that points to the first node, we advance the node and delete, till we reach the end:
+    nodeType<Type> * temp;
+    while(first != null){
+        temp = first;
+        first = first->link;
+        delete temp;
+    }
+    last = NULL;
+    count = 0;
+}
+
+// deletes any nodes in the linked list by calling destroyList()
+// this method is used by other methods when creating the linked list:
+template<class Type>
+void linkedList<Type>::initializeList(){
+    destroyList();
+}
+
+// traverse the list:
+template<class Type>
+void linkedList<Type>::print() const{
+    nodeType<Type> *current;
+    current = first; // sent the current to the first node
+    while(current != NULL){
+        std::cout << current->info << " ";
+        current << current->link;
+    }
+}
+
+// return the length of the list:
+template<class Type>
+int linkedList<Type>::length() const{
+    
+}
+
